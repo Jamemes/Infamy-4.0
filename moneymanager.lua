a78936b34f5573c4448d82901db918e733c76973
Jamemes <coderjames97@gmail.com>
2024-12-10 22:19:57 +0300

    1

--- moneymanager.lua
+++
@@ -1,3 +0,0 @@
-function MoneyManager:get_infamous_cost(rank)
-	return Application:digest_value(tweak_data.infamy.offshore_cost[rank] or tweak_data.infamy.offshore_cost[#tweak_data.infamy.offshore_cost], false)
-end
