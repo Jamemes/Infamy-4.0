3ebccc6aff24712845d2685866ee91943b45a86a
Jamemes <coderjames97@gmail.com>
2026-06-16 11:39:36 +0300

    3.4
    hudmanagerpd2 crash fix

--- hudmanagerpd2.lua
+++ hudmanagerpd2.lua
@@ -1,7 +1,7 @@
 local label = HUDManager._add_name_label
 function HUDManager:_add_name_label(data)
 	local id = label(self, data)
-	local infamy = self._hud.name_labels[id].panel:child("infamy")
+	local infamy = self._hud.name_labels and self._hud.name_labels[id] and self._hud.name_labels[id].panel:child("infamy")
 	if infamy and data.unit:base().is_husk_player then
 		local texture, pos = tweak_data.hud_icons:get_icon_data(tweak_data.infamy:rank_icon(data.unit:network():peer():rank()))
 		infamy:set_image(texture)
